e855e5e348cbeaaaa42945574e0e9294aba280ee
Nikkalis <emeraldflower15@gmail.com>
2026-09-25 18:35:01 +1200

    hey

--- db.js
+++ db.js
@@ -4,11 +4,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const { Pool, Client } = pg
 const sembiladminauth = {
-    user: process.env.SEMBIL_ADMIN_USERNAME,
-    password: process.env.SEMBIL_ADMIN_PASSWORD,
-    host: 'localhost',
-    port: 5432,
-    database: 'sembildb',
+    connectionString: process.env.DATABASE_URL,
+    ssl: {rejectUnauthorized: false}
 };
 const pool = new Pool(sembiladminauth)
 export default pool;
