9b14740bbb35debfe9f6e8a218d98c5425edaffe
Nikkalis <emeraldflower15@gmail.com>
2026-09-25 18:52:38 +1200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        account: resolve(__dirname, 'account.html'),
+        messaging: resolve(__dirname, 'messaging.html'),
+        postmake1: resolve(__dirname, 'postmake1.html'),
+        postmake2: resolve(__dirname, 'postmake2.html'),
+        postsearch: resolve(__dirname, 'postsearch.html'),
+        postview: resolve(__dirname, 'postview.html'),
+        postworkspace: resolve(__dirname, 'postworkspace.html'),
+      }
+    }
+  }
+});
